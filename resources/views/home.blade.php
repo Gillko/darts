13137d9ab70517d05d6f8636ed0113c9dc80f2ad
Gilles Vanpeteghem <gilles_vanpeteghem@hotmail.com>
2018-04-28 18:50:31 +0200

    6th Commit - changed title of home blade to darts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" />
 
-        <title>Dart app with laravel - javascript and twitter bootstrap</title>
+        <title>Darts</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
